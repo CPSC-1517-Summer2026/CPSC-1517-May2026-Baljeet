c30afe6450fda0c4c057e0362e19b63a8b59f3bf
BaljeetK79 <baljeetk@nait.ca>
2026-05-05 23:36:28 -0400

    Data Members added

---
+++ SolnOOPsDemo/OOPsDemo/Employment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemo
+{
+    public class Employment
+    {
+        // data members
+
+        #region datamembers
+
+        private string _Title;
+        private double _Years;
+        private SupervisoryLevel _Level;
+
+
+        #endregion
+
+
+
+
+        // methods
+
+        #region methods
+
+        //Employment();
+        //Employment(string Title, double Year);
+        public void Display_Employment()
+        {  }
+
+        #endregion
+
+    }
+}
