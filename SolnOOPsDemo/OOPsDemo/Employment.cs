2ff93890b6bd19c7a1ad4d3a0bfe53fef98960cd
BaljeetK79 <baljeetk@nait.ca>
2026-05-07 20:57:56 -0400

    Added comments

--- SolnOOPsDemo/OOPsDemo/Employment.cs
+++ SolnOOPsDemo/OOPsDemo/Employment.cs
@@ -9,19 +9,61 @@ namespace OOPsDemo
     public class Employment
     {
         // data members
-
         #region datamembers
 
+        //data members (aka fields, variables, attributes)
+        //typically data members are private and hold data for use
+        //  within your class
+        //usually associated with a property
+        //a data member does not have any built-in validation
         private string _Title;
         private double _Years;
         private SupervisoryLevel _Level;
 
-
         #endregion
 
 
+        #region properties
+        //Properties
+        //are associated with a single piece of data.
+        //Properties can be implemented by:
+        //  a) fully implemented property
+        //  b) auto implemented property
 
+        //A property does not need to store data
+        //  this type of property is referred to as a read-only
+        //  this property typically uses existing data values
+        //      within the instance to return a computed value
 
+        //fully implemented properties usually has additional logic
+        //  to execute for control over the data: such as validation
+        //fully implemented properties will have a declared data
+        //  member to store the data into
+
+        //auto implemented properties do not have additional logic
+        //Auto implemented properties do not have a declared
+        //  data member instead the o/s will create on the property's
+        //  behave a storage that is accessable ONLY by the property
+
+        ///<summary>
+        ///Property: Title
+        ///datatype: string
+        ///validation: there must be a character in the string
+        ///a property will always have a getter (accessor)
+        ///a property may or maynot have a setter (mutator)
+        /// no mutator the property is consider "read-only" and is
+        ///         usually returning a computed field
+        /// has a mutator, the property will at some point save the data
+        ///     to storage
+        /// the mutator may be public (default) or private
+        ///     public: accessable by outside users of the class
+        ///     private: accessable ONLY within the class, usually
+        ///                 via the constructor or a method
+        /// !!!!! a property DOES NOT have ANY declared incoming parameters !!!!!!
+        /// </summary>
+        /// 
+
+        #endregion
         // methods
 
         #region methods
