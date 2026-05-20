7b80d18c9f95cb713e57477b381411b7f052bc74
BaljeetK79 <baljeetk@nait.ca>
2026-05-19 22:15:12 -0400

    Implimented Person Should and Person class

---
+++ SolnOOPsDemo/EmploymentTest/PersonShould.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using OOPsDemo;
+
+namespace EmploymentTest
+{
+    public class PersonShould
+    {
+        #region valid test cases
+        //a Fact unit test excutes once
+        //without the [Fact] annotation, the method is NOT considered a unit test
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_The_Default_Constructor()
+        {
+            //Arrange (this is the setup of values need for doing the test)
+            string expectFirstName = "unknown";
+            string expectLastName = "unknown";
+            int expectedEmploymentPositionCount = 0;
+
+            //Act (this is the action that is under testing)
+            //sut: subject under test
+
+            Person sut = new Person();
+
+
+            // //Assert (check the results of the act against expected Values)
+
+            sut.FirstName.Should().Be(expectFirstName);
+            sut.LastName.Should().Be(expectLastName);
+            sut.Employmentpositions.Count().Should().Be(expectedEmploymentPositionCount);
+            sut.Address.Should().BeNull();
+
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_The_Greedy_Constructor()
+        { }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SolnOOPsDemo/OOPsDemo/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemo
+{
+    public class Person
+    {
+        // data members
+
+        private string _FirstName;
+        private string _LastName;
+
+        //properties
+
+        public string FirstName {
+            get {
+                return _FirstName;
+            }
+            set {
+
+                // validation
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("Firstname can't be empty");
+
+                _FirstName = value;
+            
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _LastName;
+            }
+            set
+            {
+
+                // validation
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("Lasttname can't be empty");
+
+                _LastName = value;
+
+            }
+        }
+
+        public string FullName
+        {
+            get {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public Residentaddress Address { get; set; }
+        public List<Employment> Employmentpositions {  get; set; }
+        //methods
+
+        public Person()
+        {
+            FirstName = "unknown";
+            LastName = "unknown";
+            Employmentpositions = new List<Employment>();
+        }
+
+    }
+}
