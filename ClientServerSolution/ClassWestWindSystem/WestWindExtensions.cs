ec5eb72fbadd5d50e446fd1a8ca986667c9167ff
BaljeetK79 <baljeetk@nait.ca>
2026-06-26 10:37:50 -0400

    Correction done in  WestWindExtensions.cs, Solution is correct now

--- ClientServerSolution/ClassWestWindSystem/WestWindExtensions.cs
+++ ClientServerSolution/ClassWestWindSystem/WestWindExtensions.cs
@@ -10,36 +10,48 @@ using System.Threading.Tasks;
 using ClassWestWindSystem.BLL;
 using ClassWestWindSystem.DAL;
 using ClassWestWindSystem.Entity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ClassWestWindSystem
 {
     public static class WestWindExtensions
     {
-        // setup the extension method for this library
-        public static void WWExtensions(this.IServiceCollection services,  
-                                        Action <DbContextOptionsBuilder> options)
+        //this class can hold an set of extension methods
+        //this sample creates an extension method that will add services
+        //  to IServiceCollection
+        //this method will be called from an separate project to
+        //  gain data from the WestWind database
+        //In this demo, the call will be done in the WestWindApp Program.cs file
+        //this method will do 2 things:
+        //  a) register the context connection string
+        //  b) register ALL services that I create within the BLL class(es)
+
+        public static void WWExtensionServices(this IServiceCollection services,
+            Action<DbContextOptionsBuilder> options)
         {
-            // we will register all our services that will be available for use
-            // by any system 
-            // Services will coded in BLL 
-
-            // DbContectConnection
-            // we will register the BB connection to be used by any service requiring access to the database
-
-            // Register the context service
-            // the parmeter options contain the connection string information
-
+            //handle the connection string (points to the desired database via the app lib context class)
+            //add my context class to the services (IServiceCollection)
+            //we wish it to use the string I send in on each creation of the instance of my DbContext class
             services.AddDbContext<WestWindContext>(options);
 
-            //Register your service
+            //YOU MUST REGISTER EACH AND EVERY BLL SERVICE CLASS YOU WISH TO EXPOSE TO THE WORLD
 
-
-            services.AddTransient<BuildVersion>((ServiceProvider) =>
+            //to register a service class you will use the IServiceCollection method
+            //  .AddTransient<T> where T is the service class name
+            //for any outside user coding that requires access to one or more services
+            //  you MUST register the service class, hence all public methods within the class
+            //  will be available
+            services.AddTransient<BuildVersionServices>((serviceProvider) =>
             {
-                // get the context of class that was registered above
+                //obtain the context information already registered in IServerCollections above
+                var context = serviceProvider.GetService<WestWindContext>();
 
-                var context = ServiceProvider.GetService<WestWindContext>();
-
+                //create an instance of the service class and register the said class
+                //  in IServiceCollection
+                //once the class has been registered, it can be used by ANY outside
+                //  source as long as the outside source has referenced the extension 
+                //  class method (see your Program.cs in your wb app)
+                return new BuildVersionServices(context);
             });
 
         }
