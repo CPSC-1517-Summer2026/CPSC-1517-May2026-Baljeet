76c716fa1f2b9043ba8a07bdda50b8378213372c
BaljeetK79 <baljeetk@nait.ca>
2026-06-30 23:19:17 -0400

    Appsettins.json and Program.cs updated to wire Class Library project with Web APP

--- ClientServerSolution/WestWindWebApp/Program.cs
+++ ClientServerSolution/WestWindWebApp/Program.cs
@@ -1,12 +1,32 @@
 using WestWindWebApp.Components;
+using ClassWestWindSystem;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+//retrieve the connection string from the appsetting.json
+//the connection string will be passed to the class library using
+//  the encapsulated extension method
+//the connection string will be registered to get access to the database
+var connectionstring = builder.Configuration.GetConnectionString("WWDB");
+
+//setup the registration of the services to be available for use
+//  by this web application
+//the technique used in this example has the registrations encapsulated
+//  within the class library via an extension method
+//technically, you could put all the setup that is in the extension method
+//  here in this file
+builder.Services.WWExtensionServices(options => options.UseSqlServer(connectionstring));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
