4022c4ab951a2356716bda33c669a6d470b0df12
BaljeetK79 <baljeetk@nait.ca>
2026-07-02 21:46:17 -0400

    Refactoring Region Queries page and  Service

--- ClientServerSolution/ClassWestWindSystem/WestWindExtensions.cs
+++ ClientServerSolution/ClassWestWindSystem/WestWindExtensions.cs
@@ -67,6 +67,19 @@ namespace ClassWestWindSystem
                 return new RegionServices(context);
             });
 
+            services.AddTransient<CategoriesProductsServices>((serviceProvider) =>
+            {
+                //obtain the context information already registered in IServerCollections above
+                var context = serviceProvider.GetService<WestWindContext>();
+
+                //create an instance of the service class and register the said class
+                //  in IServiceCollection
+                //once the class has been registered, it can be used by ANY outside
+                //  source as long as the outside source has referenced the extension 
+                //  class method (see your Program.cs in your wb app)
+                return new CategoriesProductsServices(context);
+            });
+
         }
 
        
